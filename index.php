30910a3d5112f3c90746f5bd3388fe326201ad36
Ram Ochoa <ram.ochoa.hes@gmail.com>
2017-02-08 19:21:13 -0600

    added quotes generator

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
+<?php
+$lines = explode("\n", file_get_contents('quotes.txt'));
+$line = $lines[mt_rand(0, count($lines) - 1)];
+list($text, $author) = explode('|', $line);
+?>
 
-<h1>Hello File</h1>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+
+</head>
+
+<body>
+<h1>Ram Ochoa</h1>
 
 <img src='/images/me.jpg' alt='me'>
+
+<p>
+Indtroduction. 
+</p>
+
+<h2>Random Quote</h2>
+		
+<blockquote class="text-center">
+		<p><?php echo $text; ?></p>
+		<footer><?php echo $author; ?></footer>
+</blockquote>
+
+</body>
+</html>
